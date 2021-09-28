6f911e17a702d22d1242898ba1e7c78d98fceb59
JBrady <jbradysd@icloud.com>
2021-09-28 15:16:52 -0700

    Getting an error TypeError: _this.props.handleGamePlay is not a function

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-class Square extends Component{
-  render(){
-    return(
+class Square extends Component {
+  handleClick = () => {
+    this.props.handleGamePlay(this.props.index)
+  }
+  render() {
+    return (
       <>
+        <div 
+        className="square"
+        onClick={this.handleClick}
+        >
+          {this.props.value}
+        </div>
       </>
     )
   }
 }
+
 export default Square
